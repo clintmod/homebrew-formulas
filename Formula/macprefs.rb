3a5f10495e5f5ca808dd1c50cb1644b5e9466dac
Clint Modien <cmodien@gmail.com>
2017-11-14 17:17:45 -0600

    fixing macprefs

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -1,13 +1,15 @@
 class Macprefs < Formula
+  include Language::Python::Virtualenv
+
   desc "Backup your Mac System and App Preferences"
   homepage "https://github.com/clintmod/macprefs"
-  url "https://github.com/clintmod/macprefs/archive/v1.0.0.tar.gz"
-  sha256 "a6fd2a7f0e1ed117b212f3228ada8a48b8dea78fbe291472ce0bb9a43ee84fd1"
+  url "https://github.com/clintmod/macprefs/archive/v1.0.1.tar.gz"
+  sha256 "617ba8ba50fabdebf1b9ce846bea83a0ded3ed209ac55aedcc2c842e52099545"
 
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    bin.install_symlink "bin/macprefs"
+    bin.install Dir["*"]
   end
 
   test do
