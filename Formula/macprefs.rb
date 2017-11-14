bead0dc205a95e1565aa8b4e85f5e49896ee4cf6
Clint Modien <cmodien@gmail.com>
2017-11-14 14:19:16 -0600

    adding macprefs

---
+++ Formula/macprefs.rb
@@ -0,0 +1,16 @@
+class Macprefs < Formula
+  desc "Backup your Mac System and App Preferences"
+  homepage "https://github.com/clintmod/macprefs"
+  url "https://github.com/clintmod/macprefs/releases/tag/v1.0.0"
+  sha256 "baa17f271b2772299b380a87bb8cd7dc1c4c9b936fc6da1794455e3b3a327c96"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    bin.install Dir[libexec/"bin/*"]
+  end
+
+  test do
+    system "#{bin}/macprefs", "--help"
+  end
+end
