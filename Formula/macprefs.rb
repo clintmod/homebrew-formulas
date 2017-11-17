ad6a6b363eb59460308214a57ec656a57475b6f9
Clint M <cmodien@gmail.com>
2017-11-17 15:45:50 -0600

    Updating to version v1.0.3

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -1,18 +1,19 @@
 class Macprefs < Formula
-  include Language::Python::Virtualenv
-
-  desc "Backup your Mac System and App Preferences"
-  homepage "https://github.com/clintmod/macprefs"
-  url "https://github.com/clintmod/macprefs/archive/v1.0.1.tar.gz"
-  sha256 "617ba8ba50fabdebf1b9ce846bea83a0ded3ed209ac55aedcc2c842e52099545"
-
-  depends_on :python if MacOS.version <= :snow_leopard
-
-  def install
-    bin.install Dir["*"]
+    include Language::Python::Virtualenv
+  
+    desc "Backup and Restore your Mac System and App Preferences"
+    homepage "https://github.com/clintmod/macprefs"
+    url "https://github.com/clintmod/macprefs/archive/v1.0.3.tar.gz"
+    sha256 "8c9ed960586410d4c5925931924b646e99f3305ce41c0dd14d3337fcf09415ab"
+  
+    depends_on :python if MacOS.version <= :snow_leopard
+  
+    def install
+      bin.install Dir["*"]
+    end
+  
+    test do
+      system "#{bin}/macprefs", "--help"
+    end
   end
-
-  test do
-    system "#{bin}/macprefs", "--help"
-  end
-end
+  
