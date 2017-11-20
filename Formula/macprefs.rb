f42b735e00ed480238cfc4cfe7a8618f4042273e
Clint M <cmodien@gmail.com>
2017-11-19 21:09:36 -0600

    Updating to version v1.0.14

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -3,8 +3,8 @@ class Macprefs < Formula
   
     desc "Backup and Restore your Mac System and App Preferences"
     homepage "https://github.com/clintmod/macprefs"
-    url "https://github.com/clintmod/macprefs/archive/v1.0.13.tar.gz"
-    sha256 "f10e8e470ebc41a20c3315818247d494743c0e7fc13fa6afe576393af2e8f58d"
+    url "https://github.com/clintmod/macprefs/archive/v1.0.14.tar.gz"
+    sha256 "83301d652c359c23abf0c141bb183bc5a81e7825e7e8ebd4449950464857eda0"
   
     depends_on :python if MacOS.version <= :snow_leopard
   
